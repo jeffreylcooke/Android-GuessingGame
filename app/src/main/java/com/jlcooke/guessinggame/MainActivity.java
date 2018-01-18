64c5f7af24443ce08e48dba6fefce027b3fb4328
Jeffrey Cooke <jeffrey.l.cooke@gmail.com>
2018-01-18 09:43:41 -0500

    Initial commit page 112 prior to Responding to User Selections

---
+++ app/src/main/java/com/jlcooke/guessinggame/MainActivity.java
@@ -0,0 +1,104 @@
+package com.jlcooke.guessinggame;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txtGuess;
+    private Button btnGuess;
+    private TextView lblOutput;
+    private int theNumber;
+    private int numGuesses;
+
+    public void checkGuess() {
+        String guessText = txtGuess.getText().toString();
+        String message = "";
+        try {
+            int guess = Integer.parseInt(guessText);
+            numGuesses++;
+            if (guess < theNumber)
+                message = guess + " is too low. Please try again.";
+            else if (guess > theNumber)
+                message = guess + " is too high. Please try again.";
+            else {
+                message = guess + " is CORRECT! You won in " + numGuesses + " guesses. Let's play again!";
+                newGame();
+            }
+        } catch (Exception e) {
+            message = "Enter a whole number between 1 and 100!";
+        } finally {
+            lblOutput.setText(message);
+            txtGuess.requestFocus();
+            txtGuess.selectAll();
+        }
+    }
+
+    public void newGame() {
+        theNumber = (int)(Math.random() * 100 + 1);
+        numGuesses = 0;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtGuess = (EditText) findViewById(R.id.txtGuess);
+        btnGuess = (Button) findViewById(R.id.btnGuess);
+        lblOutput = (TextView) findViewById(R.id.lblOutput);
+        newGame();
+        btnGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkGuess();
+            }
+        });
+        txtGuess.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                checkGuess();
+                return true;
+            }
+        });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
