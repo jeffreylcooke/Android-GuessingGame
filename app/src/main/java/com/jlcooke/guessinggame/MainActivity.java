d6639a812e12ef89f5feb02a6bef1efc9d5334e9
Jeffrey Cooke <jeffrey.l.cooke@gmail.com>
2018-01-18 12:54:28 -0500

    Added Settings menu function. Still missing Game Stats.

--- app/src/main/java/com/jlcooke/guessinggame/MainActivity.java
+++ app/src/main/java/com/jlcooke/guessinggame/MainActivity.java
@@ -1,8 +1,11 @@
 package com.jlcooke.guessinggame;
 
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView lblOutput;
     private int theNumber;
     private int numGuesses;
+    private int range = 100; // user selectable guessing range
+    private TextView lblRange; // prompt for user based on range selection
 
     public void checkGuess() {
         String guessText = txtGuess.getText().toString();
@@ -36,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 newGame();
             }
         } catch (Exception e) {
-            message = "Enter a whole number between 1 and 100!";
+            message = "Enter a whole number between 1 and " + range + "!";
         } finally {
             lblOutput.setText(message);
             txtGuess.requestFocus();
@@ -45,7 +50,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void newGame() {
-        theNumber = (int)(Math.random() * 100 + 1);
+        theNumber = (int)(Math.random() * range + 1);
+        lblRange.setText("Enter a number between 1 and " + range + ".");
+        txtGuess.setText("" + range/2);
+        txtGuess.requestFocus();
+        txtGuess.selectAll();
         numGuesses = 0;
     }
 
@@ -53,9 +62,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtGuess = (EditText) findViewById(R.id.txtGuess);
-        btnGuess = (Button) findViewById(R.id.btnGuess);
-        lblOutput = (TextView) findViewById(R.id.lblOutput);
+        txtGuess = findViewById(R.id.txtGuess);
+        btnGuess = findViewById(R.id.btnGuess);
+        lblOutput = findViewById(R.id.lblOutput);
+        lblRange = findViewById(R.id.textView2);
         newGame();
         btnGuess.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,10 +80,10 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -93,12 +103,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                final CharSequence[] items = {"1 to 10", "1 to 100", "1 to 1000"};
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Select the Range:");
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int item) {
+                        switch(item) {
+                            case 0:
+                                range = 10;
+                                newGame();
+                                break;
+                            case 1:
+                                range = 100;
+                                newGame();
+                                break;
+                            case 2:
+                                range = 1000;
+                                newGame();
+                                break;
+                        }
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog alert = builder.create();
+                alert.show();
+                return true;
+            case R.id.action_newgame:
+                newGame();
+                return true;
+            case R.id.action_gamestats:
+                return true;
+            case R.id.action_about:
+                AlertDialog aboutDialog = new AlertDialog.Builder(MainActivity.this).create();
+                aboutDialog.setTitle("About: Guessing Game");
+                aboutDialog.setMessage("(c) 2018 Jeffrey Cooke\n\nExample Guessing Game from\n'Learn Java the Easy Way'\nby Bryson Payne");
+                aboutDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                aboutDialog.show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
